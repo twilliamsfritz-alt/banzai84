f28f36ab15f210e6da3dfa8aecac1cc131995413
twilliamsfritz-alt <twilliamsfritz@gmail.com>
2026-06-30 11:01:41 -0300

    force rebuild mar 30/06/2026 11:01:40,60

--- app.py
+++ app.py
@@ -5645,3 +5645,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_ENV", "production") == "development"
     app.run(host="0.0.0.0", port=port, debug=debug)
+  
